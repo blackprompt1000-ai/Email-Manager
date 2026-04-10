acf5404fa2e4d906ca643f3d4d69b9230b58464d
GRANTH AGARWAL <black.prompt.1000@gmail.com>
2026-04-10 21:27:50 +0530

    Delete hackathon directory

--- hackathon/email_rectifier/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Packaged OpenEnv entry points for Email Rectifier."""
-
-from .environment import CATEGORY_IDEAL_ACTIONS, LIFE_MODE_MULTIPLIERS, EmailEnv
-
-__all__ = ["CATEGORY_IDEAL_ACTIONS", "LIFE_MODE_MULTIPLIERS", "EmailEnv"]
--- hackathon/openenv_agent.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility wrapper for the packaged EmailEnv implementation."""
-
-from email_rectifier import CATEGORY_IDEAL_ACTIONS, LIFE_MODE_MULTIPLIERS, EmailEnv
-
-__all__ = ["CATEGORY_IDEAL_ACTIONS", "LIFE_MODE_MULTIPLIERS", "EmailEnv"]
